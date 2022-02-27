3b059d8413b899d1c40ab34c5a89618d938b98e6
arturmzf <artur.mzf@gmail.com>
2022-02-27 21:47:51 +0300

    Модуль 2 | Лабораторная работа 02

--- mod2/file.php
+++ mod2/file.php
@@ -1,46 +1,96 @@
 <?php
-/*
-ЗАДАНИЕ 1
-- Установите константу для хранения имени файла
-- Проверьте, отправлялась ли форма и корректно ли отправлены данные из формы
-- В случае, если форма была отправлена, отфильтруйте полученные значения
-- Сформируйте строку для записи с файл
-- Откройте соединение с файлом и запишите в него сформированную строку
-- Выполните перезапрос текущей страницы (чтобы избавиться от данных, отправленных методом POST)
-*/
+    /*
+    ЗАДАНИЕ 1
+    - Установите константу для хранения имени файла
+    - Проверьте, отправлялась ли форма и корректно ли отправлены данные из формы
+    - В случае, если форма была отправлена, отфильтруйте полученные значения
+    - Сформируйте строку для записи с файл
+    - Откройте соединение с файлом и запишите в него сформированную строку
+    - Выполните перезапрос текущей страницы (чтобы избавиться от данных, отправленных методом POST)
+    */
+
+    define(FILENAME, "fl1.txt");
+    $firstName = "";
+    $lastName = "";
+    $result = "";
+
+    // if($_SERVER["REQUEST_METHOD"] == "POST"){|
+    if(isset($_POST["fname"]) && $_POST["lname"]) {
+        $firstName = trim(strip_tags($_POST["fname"]));
+        $lastName = trim(strip_tags($_POST["lname"]));
+
+        $result = $firstName." ".$lastName."\n";
+
+        file_put_contents(FILENAME, $result, FILE_APPEND);
+
+        header("LOCATION: file.php");
+        exit(); // Лишнее же... Ну, да ладно...
+    }
+
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Работа с файлами</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
-<head>
-	<title>Работа с файлами</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
-</head>
-<body>
+        <h1>Заполните форму</h1>
 
-<h1>Заполните форму</h1>
+        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            Имя: <input type="text" name="fname" /><br />
+            Фамилия: <input type="text" name="lname" /><br />
 
-Имя: <input type="text" name="fname" /><br />
-Фамилия: <input type="text" name="lname" /><br />
+            <br />
 
-<br />
+            <input type="submit" value="Отправить!" />
 
-<input type="submit" value="Отправить!" />
+        </form>
 
-</form>
+        <?php
+            /*
+            ЗАДАНИЕ 2
+            - Проверьте, существует ли файл с информацией о пользователях
+            - Если файл существует, получите все содержимое файла в виде массива строк
+            - В цикле выведите все строки данного файла с порядковым номером строки
+            - После этого выведите размер файла в байтах.
+            */
+            $resultArray = array();
+            if(file_exists(FILENAME)){
+                $resultArray = file(FILENAME);
 
-<?php
-/*
-ЗАДАНИЕ 2
-- Проверьте, существует ли файл с информацией о пользователях
-- Если файл существует, получите все содержимое файла в виде массива строк
-- В цикле выведите все строки данного файла с порядковым номером строки
-- После этого выведите размер файла в байтах.
-*/
-?>
+                // Вывод 1
+                echo("<br />");
+                $i = 1;
+                foreach($resultArray as $data){
+                    echo($i." ".$data."<br />");
+                    $i++;
+                }
 
-</body>
+                // Вывод 2 - то же, что и 3, только самописно :)
+                if(is_array($resultArray)){
+                    echo("<br />");
+                    for($j = sizeof($resultArray); $j > 0; $j--) {
+                        echo((sizeof($resultArray) - $j + 1) . " " . $resultArray[$j - 1] . "<br />");
+                    }
+                }
+
+                // Вывод 3 - то же, что и 2, только с помощью PHP
+                echo("<br />");
+                $resultArrayReverse = array_reverse($resultArray);
+                $k = 1;
+                foreach($resultArrayReverse as $data){
+                    echo($k." ".$data."<br />");
+                    $k++;
+                }
+
+                echo("<br />");
+                echo("Размер файла составляет: ".filesize(FILENAME)." байт.");
+            }
+        ?>
+
+    </body>
 </html>
